fdc5653fa263f0cfeca14e29e7c3d81f41976d43
Asier Carreño <ascalotoru@gmail.com>
2016-06-01 10:27:56 +0200

    Hay rosa added

---
+++ hay_rosa.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Author: Asier Carreño
+# Email: ascalotoru [at] gmail [dot] com
+# Busca en la tienda de BQ y comprueba que haya stock del BQ Aquaris X5 Rosa
+
+import urllib2
+import smtplib
+import time
+from pushbullet import Pushbullet
+from bs4 import BeautifulSoup
+
+SMTP_Server = ""
+SMTP_Port = ""
+SMTP_User = ""
+SMTP_Password = ""
+
+PUSH_APIKEY = ""
+
+fromaddr = ""
+toaddrs = ""
+subject = "Hay rosa en la tienda de BQ"
+msg = "Hay rosa. Corre!!!!"
+
+def mandar_email():
+	servidor = smtplib.SMTP(SMTP_Server, SMTP_Port)
+	servidor.starttls()
+	servidor.login(SMTP_User, SMTP_Password)
+	servidor.sendmail(fromaddr, toaddrs, msg)
+	servidor.quit()
+
+def mandar_push():
+	p = Pushbullet(PUSH_APIKEY)
+	p.push_note("Hay rosa!", "Corre que hay rosa.")
+
+print time.strftime("%a %H:%M:%S") + " hay_rosa: Me inicio"
+
+openurl = urllib2.urlopen("https://store.bq.com/es/aquaris-x5")
+soup = BeautifulSoup(openurl, "html.parser")
+sp = soup.find_all("label", "inline_element")
+for s in sp:
+	nombre = s.contents[0].string.encode("utf-8")
+	if nombre.lower().find("rosa") != -1:
+		mandar_email()
+		mandar_push()
